be4d3fa969d2c6b830a49cd269d7a1fce2620fb3
ostlej61 <ostlej61@uwosh.edu>
2020-11-18 14:45:50 -0600

    Added Procfile and package.json, changed PORT to web server.

--- Assignment2WebServer.js
+++ Assignment2WebServer.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
@@ -23,6 +24,6 @@ http.createServer(function(req, res) {
 		return res.end();
 	});
 	
-}).listen(8080);
+}).listen(PORT);
 
-console.log("Server Listening on Port 8080...");
+console.log("Server Listening on assigned PORT...");
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node assignment2webserver.js
